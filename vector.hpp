3e8213e0894fbd6394160a9fdd0ee1364cf6a8c9
mbjaghou <mouhamedbj61@gmail.com>
2022-11-20 11:46:14 +0100

    vector

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: mbjaghou <mbjaghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 11:53:15 by mbjaghou          #+#    #+#             */
-/*   Updated: 2022/11/19 14:31:42 by mbjaghou         ###   ########.fr       */
+/*   Updated: 2022/11/20 11:32:46 by mbjaghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,28 +28,45 @@ class vector
         typedef std::ptrdiff_t                                      difference_type;
         typedef value_type&                                         reference;
         typedef const value_type&                                   const_reference;
-        typedef Allocator::pointer                                  pointer;
-        typedef Allocator::const_pointer                            const_pointer;
+        typedef typename Allocator::pointer                         pointer;
+        typedef typename Allocator::const_pointer                   const_pointer;
         typedef ft::random_access_iterator<value_type>              iterator;
         typedef ft::random_access_iterator<const value_type>        const_iterator;
         typedef ft::reverse_iterator<value_type>                    reverse_iterator;
         typedef ft::reverse_iterator<const value_type>              const_reverse_iterator;
         /*--------Member functions-----*/
-        vector(){}
-        explicit vector( const Allocator& alloc ){}
-        explicit vector( size_type count, const T& value = T(), const Allocator& alloc = Allocator()){}
-        template< class InputIt >
-        vector( InputIt first, InputIt last, const Allocator& alloc = Allocator() ){}
-        vector( const vector& other );
+        explicit vector (const allocator_type& alloc = allocator_type()){}
+        explicit vector (size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type()){}
+        template <class InputIterator>
+            vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()){}
+        vector (const vector& x){}
         ~vector(){}
         vector& operator=( const vector& other ){}
-        void assign( size_type count, const T& value ){}
-        template< class InputIt >
-        void assign( InputIt first, InputIt last ){}
-        allocator_type get_allocator() const{}
+        allocator_type get_allocator() const{return (this->_alloc);}
+        /*--------Iterators-----*/
+        iterator begin(){return (iterator(p));}
+        const_iterator begin() const{return const_iterator(p);}
+        iterator end(){return (iterator(p + _size));}
+        const_iterator end() const{return (const_iterator(p + _size));}
+        reverse_iterator rbegin(){return (reverse_iterator(p + _size));}
+        const_reverse_iterator rbegin() const{return (const_reverse_iterator(p + _size));}
+        reverse_iterator rend(){return reverse_iterator(p);}
+        const_reverse_iterator rend() const{return (const_reverse_iterator(p));}
+        /*--------Capacity-----*/
+        bool empty() const{
+            if(this->_size == 0)
+                return (1);
+            return (0);
+        }
+        size_type size() const{return (this->_size);}
+        size_type max_size() const{}
+        void reserve( size_type new_cap ){}
+        size_type capacity() const{return (this->_capacity);};
   
     private:
-        size_t size;
-        size_t capacity;
+        size_type           _size;
+        size_type           _capacity;
+        pointer             p;
+        allocator_type      _alloc;
 };
 #endif
